efa11246c553a7f38915bcc7dde782725d9d312c
Kuni <kuni.ujiie@grapecity.com>
2024-04-12 14:10:46 +0900

    各ライブラリを最新バージョンに更新

--- quickstart/src/index.js
+++ quickstart/src/index.js
@@ -1,4 +1,4 @@
-import * as gcpdfviewer from '@grapecity/gcpdfviewer';
+import * as gcpdfviewer from '@mescius/dspdfviewer';
 import repQuickStartSample from './pdfs/diodocs_pricelist.pdf';
 
 var viewer = new gcpdfviewer.GcPdfViewer('#pdfviewer', { supportApi: 'https://localhost:7224/api/pdf-viewer' });
